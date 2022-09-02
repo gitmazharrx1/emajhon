c58134a5daa2a3db2a246dda9e6d5e4ca135ad01
Mazharul Islam <mazharulislam.rx1@gmail.com>
2022-09-02 09:09:42 -0700

    Grand Total Done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import './Cart.css'
 
-const Cart = ({cart}) => {
+const Cart = (props) => {
+    const { cart } = props;
+    let total = 0;
+    for (const product of cart) {
+        total = total + product.price;
+    }
+    let shipping = 0;
+    for (const product of cart) {
+        shipping = shipping + product.shipping;
+    }
+    const taxvalue = total * 0.1;
+
+    const tax = taxvalue.toFixed(2);
+
+    const grandTotal = parseInt(total) + parseInt(shipping) +parseFloat(tax);
+
     return (
-        <div>
-            <h1>Order Summary</h1>
+        <div className='cart'>
+            <h2>Order Summary</h2>
             <p>Selected Items: {cart.length}</p>
+            <p>Total Price: {total}</p>
+            <p>Total Shipping Charge: {shipping}</p>
+            <p>Tax: {tax}</p>
+            <h3>Grand Total: {grandTotal}</h3>
         </div>
     );
 };
